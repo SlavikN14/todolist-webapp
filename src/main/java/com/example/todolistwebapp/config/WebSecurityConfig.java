4e19ef78f49bc7030e3ab112b4d106bda13a53bc
Viacheslav Nykonchuk <slavir186@gmail.com>
2023-05-11 13:11:20 +0300

    update to spring-boot version 2.7.11

--- src/main/java/com/example/todolistwebapp/config/WebSecurityConfig.java
+++ src/main/java/com/example/todolistwebapp/config/WebSecurityConfig.java
@@ -1,25 +1,25 @@
 package com.example.todolistwebapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**").permitAll()
-                    .anyRequest().authenticated()
+                .authorizeRequests(authorize -> authorize.antMatchers("/", "/static/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
                 .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/")
-                    .permitAll()
-                .and()
-                    .logout().permitAll();
+                .logout().permitAll();
+        return http.build();
     }
 }
